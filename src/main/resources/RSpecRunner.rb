eb52716f071098561a5cdc5ed18ca5f653c500fc
André Ribeiro Gonçalves <andre.rigon@gmail.com>
2010-10-28 17:28:03 -0200

    refactoring of runner ruby script

--- src/main/resources/RSpecRunner.rb
+++ src/main/resources/RSpecRunner.rb
@@ -1,12 +1,14 @@
 require 'rubygems'
 require 'rspec/core'
 
+class RSpecRunnerWithArgsFix <  RSpec::Core::Runner
+    def self.autorun_with_args(*args)
+      return if autorun_disabled? || installed_at_exit? || running_in_drb?
+      @installed_at_exit = true 
+      run(args, $stderr, $stdout)
+    end
+end
+
 def run(sourceDir, reportFile)
-	RSpec::Core::Runner.module_eval """
-	 	def self.autorun_with_args(args)
-			return if autorun_disabled? || installed_at_exit? || running_in_drb?
-			@installed_at_exit = true 
-			run(args, $stderr, $stdout)
-		end"""
-	RSpec::Core::Runner.autorun_with_args([sourceDir, '-f', 'html', '-o', reportFile])
+  RSpecRunnerWithArgsFix.autorun_with_args( sourceDir, '-f', 'html', '-o', reportFile )
 end
